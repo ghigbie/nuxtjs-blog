f2823c0be6a30ec3c715fec43253a0919e0dae79
ghigbie <gehigbie@gmail.com>
2019-02-16 20:06:46 -0700

    Add detail to post page

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="single-post-page">
+        <section class="post">
+            <h1 class="post-title">{{ this.title }}</h1>
+            <div class="post-details">
+                <div class="last-updated">{{ this.date }}</div>
+                <div class="author">{{ this.author }}</div>
+            </div>
+            <p class="content">{{ this.postContent }}</p>
+        </section>
+        <section class="post-feedback">
+            {{ this.feedback }}
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        title(){
+            return 'Title of Post';
+        },
+        author(){
+            return 'Author of Post';
+        },
+        date(){
+            return 'Date of Last Updated';
+        },
+        postContent(){
+            return 'Content of post';
+        },
+        feedback(){
+            return 'Feedback on post';
+        }
+
+    }
+}
+</script>
+
+
+<style>
+
+</style>
