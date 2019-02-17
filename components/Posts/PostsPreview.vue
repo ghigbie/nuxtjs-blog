2925bc3c02624529d1270a38ac43bda948a9e93e
ghigbie <gehigbie@gmail.com>
2019-02-17 15:58:17 -0700

    Add post preview component

---
+++ components/Posts/PostsPreview.vue
@@ -0,0 +1,13 @@
+<template>
+    <nuxt-link :to="'/posts' + 2" class="post-preview">
+        <article>
+          <div class="post-thumbnail"
+               style="background-image: url('https://www.topuniversities.com/sites/default/files/articles/lead-images/mit_vs_caltech.jpg')">
+          </div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+    </nuxt-link>
+</template>
