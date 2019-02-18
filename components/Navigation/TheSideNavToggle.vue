00de9d91cd438f52ff72db36d7b2cec14f576aa8
ghigbie <gehigbie@gmail.com>
2019-02-17 19:09:13 -0700

    Add new nav item

---
+++ components/Navigation/TheSideNavToggle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="drawer-toggle"
+         role="button"> 
+         <div class="bar"></div>
+         <div class="bar"></div>
+         <div class="bar"></div>
+    </div>
+</template>
+
+<style>
+.drawer-toggle{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 50%;
+    width: 35px;
+    cursor: pointer;
+}
+</style>
